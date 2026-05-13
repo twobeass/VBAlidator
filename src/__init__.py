5a29973dd7e4c1f23912f8ad13e2f50966be86ae
semantic-release <semantic-release>
2026-05-13 13:11:29 +0000

    chore(release): v1.3.5
    [skip ci]

--- src/__init__.py
+++ src/__init__.py
@@ -6,7 +6,7 @@ Public API
 >>> result = precheck("MyModule.bas", host="excel")
 >>> result.compile_safe, result.score
 """
-__version__ = "1.3.4"
+__version__ = "1.3.5"
 
 from .api import PrecheckResult, precheck, precheck_source
 
